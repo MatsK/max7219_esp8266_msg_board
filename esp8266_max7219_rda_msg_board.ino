1ba8579e9b8f96a7fc28386a7499c1225ca7f683
Mats Karlsson <mats.o.karlsson@gmail.com>
2025-04-13 12:49:38 +0200

    Update esp8266_max7219_rda_msg_board.ino

--- esp8266_max7219_rda_msg_board.ino
+++ esp8266_max7219_rda_msg_board.ino
@@ -1,7 +1,6 @@
-//MAX7219 RDA Message Board Version
-#define VERSION "v2022.08.15.4m-nmcu"
-//Tested on "ESP8266 Boards (3.0.2) / NodeMCU 1.0 (ESP12E-Module)"
-//Also tested on "D1 Mini"
+// MAX7219 RDA Message Board Version
+// Tested on "ESP8266 Boards (3.0.2) / NodeMCU 1.0 (ESP12E-Module)"
+// Also tested on "Wemos/LoLin D1 Mini"
 
 #include <MD_MAX72xx.h>
 #include <MD_Parola.h>
@@ -14,6 +13,8 @@
 #include <LittleFS.h>
 #include <PubSubClient.h>
 #include "01_Shared.h"
+
+#define VERSION "v2025.04.11"
 
 void setup() {
   clientId = clientIdPrefix + chipId;// + clientIdTrail;
@@ -56,7 +57,7 @@ void setup() {
   // Display initialisation
   P.begin();
 
-  //P.addChar('¬', degC);
+  P.addChar('¬', degC);
   //P.addChar('&', degF);
   //P.addChar('~', waveSine);
   //P.addChar('+', waveSqar);
@@ -64,54 +65,54 @@ void setup() {
 
   curMessage[0] = newMessage[0] = '\0';
 
-  WiFi.mode(WIFI_STA); // explicitly set mode, esp defaults to STA+AP
+  WiFi.mode(WIFI_STA);  // Explicitly set mode, esp defaults to STA+AP
   
-  WiFiManager wm; //WiFiManager, Local intialization. Once its business is done, there is no need to keep it around
+  WiFiManager wm;       // WiFiManager, Local intialization. Once its business is done, there is no need to keep it around
 
-  //Comment out and edit line below if you want to change AP Config Portal IP from default 192.168.4.1 to something else
+  // Comment out and edit line below if you want to change AP Config Portal IP from default 192.168.4.1 to something else
   //wm.setAPStaticIPConfig(IPAddress(192,168,100,1), IPAddress(192,168,100,1), IPAddress(255,255,255,0));
 
-  //wm.setSTAStaticIPConfig(IPAddress(192,168,1,92), IPAddress(192,168,1,1), IPAddress(255,255,255,0), IPAddress(192,168,1,1)); // optional DNS 4th argument
+  //wm.setSTAStaticIPConfig(IPAddress(192,168,1,92), IPAddress(192,168,1,1), IPAddress(255,255,255,0), IPAddress(192,168,1,1));  // Optional DNS 4th argument
 
   wm.setHostname(clientId);
 
-  //run code before wifi setup is checked/started
+  // Run code before WiFi setup is checked/started
   wm.setAPCallback(configModeCallback);
-  wm.setClass("invert"); // dark theme
+  wm.setClass("invert");  // Dark theme
 
-  //confiugre WifiManager portal (essentially specifying the button we want on the captive portal page)
+  // Confiugre WifiManager portal (essentially specifying the button we want on the captive portal page)
   std::vector<const char *> wm_menu  = {"wifi", "info", "update"};
   wm.setShowInfoUpdate(true);
   wm.setShowInfoErase(true);
   wm.setMenu(wm_menu);
 
-  //try to connect for 60 seconds before starting config portal
+  // Try to connect for 60 seconds before starting config portal
   wm.setConnectTimeout(60);
 
   bool res;
-  // res = wm.autoConnect(); // auto generated AP name from chipid
-  // res = wm.autoConnect("AutoConnectAP"); // anonymous ap
-  res = wm.autoConnect(ap_mode_ssid,ap_mode_password); // Start AP either in setup mode or connects to configured wifi network
+  // res = wm.autoConnect();                               // Auto generated AP name from chipid
+  // res = wm.autoConnect("AutoConnectAP");                // Anonymous ap
+  res = wm.autoConnect(ap_mode_ssid,ap_mode_password);  // Start AP either in setup mode or connects to configured wifi network
   if(!res) {
     Serial.println("Failed to connect");
-  } 
+  }
   else {
-    //if you get here you have connected to the WiFi    
+    // If you get here you have connected to the WiFi
     Serial.println("connected...yeey :)");
     firstMessage = true;
     if(afterWiFiConfig){
       ESP.restart();
     }
   }
-    
-  //show ip address on serial
+
+  // Show ip address on serial
   sprintf(assignedIP, "%01d.%01d.%01d.%01d", WiFi.localIP()[0], WiFi.localIP()[1], WiFi.localIP()[2], WiFi.localIP()[3]);
   PRINT("\nAssigned IP: ", assignedIP);
   PRINTS("\n\n");
 
   httpWebDirDef();
 
-  //Scroll first message in message mode
+  // Scroll first message in message mode
   sprintf(newMessage, "Wifi Message Mode - Hostname: %s - IP: %s - http Username: %s - Password: %s - Version: %s", clientId.c_str(), assignedIP, web_username, web_password, VERSION);
   startupBuzzer();
   displaySilentMsg();
@@ -123,23 +124,17 @@ void setup() {
   else {
     PRINTS("\n\nError setting up MDNS responder!");
   }
-
   initMqtt();
 }
 
 
 void loop() {
-
   MDNS.update();
-
   handleHttpServer();
-
   checkMqtt();
-
   scrollTextParola(); 
 
 #if ENABLE_FLASH_BUTTON
   flash_button.read();
 #endif
-
 }
