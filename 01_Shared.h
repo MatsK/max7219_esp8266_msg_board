3397f9e2d5f05982b98fddc2d38bb4109776b5f3
Mats Karlsson <mats.o.karlsson@gmail.com>
2025-04-13 12:44:02 +0200

    Update 01_Shared.h

--- 01_Shared.h
+++ 01_Shared.h
@@ -1,13 +1,21 @@
 // Define the number of devices we have in the chain and the hardware interface
-// NOTE: These pin numbers will probably not work with your hardware and may
-// need to be adapted
-#define MAX_DEVICES 4
-#define CLK_PIN D5 // or SCK
-#define DATA_PIN D7 // or MOSI
-// CS_PIN D8 for NodeMCU 1.0 (ESP12E-Module)
-#define CS_PIN D8
+// NOTE: These pin numbers will probably not work with your hardware and may need to be adapted
+#define MAX_DEVICES   4
+#define CLK_PIN       D5  // or SCK
+#define DATA_PIN      D7  // or MOSI
+#define CS_PIN        D6  // CS_PIN D8 for NodeMCU 1.0 (ESP12E-Module)
 
-// Edit LED matrix hardware type
+#define BUZZER        D1  // Define pin for a buzzer
+
+#define FLASH_BUTTON  D2  // Button for restoring default settings
+
+#define VERSION         "v2025.04.11"
+
+// Select one of the below MAX7219 hardware types:
+//   - PAROLA_HW
+//   - GENERIC_HW
+//   - ICSTATION_HW
+//   - FC16_HW
 #define HARDWARE_TYPE MD_MAX72XX::FC16_HW
 
 // HARDWARE SPI
@@ -15,19 +23,16 @@ MD_Parola P = MD_Parola(HARDWARE_TYPE, CS_PIN, MAX_DEVICES);
 // SOFTWARE SPI
 //MD_Parola P = MD_Parola(HARDWARE_TYPE, DATA_PIN, CLK_PIN, CS_PIN, MAX_DEVICES);
 
-// Define pin where the buzzer is connected
-#define BUZZER D1
-
-//WifiManager use mdns
+// WifiManager use mdns
 #define WM_MDNS 1
 #define WM_DEBUG_LEVEL 0
 
-//DEBUG ON OR OFF
+// DEBUG ON OR OFF
 #define DEBUG 0
 #define PRINT_CALLBACK  0
 #define PAUSE_TIME  0
 
-//Flash button enabled/disaled
+// Flash button enabled/disaled
 #define ENABLE_FLASH_BUTTON 0
 
 #if ENABLE_FLASH_BUTTON
@@ -37,36 +42,35 @@ MD_Parola P = MD_Parola(HARDWARE_TYPE, CS_PIN, MAX_DEVICES);
   EasyButton flash_button(FLASH_BUTTON);
 #endif
 
-
 // You must enable either "HTTP_SERVER" or "HTTPS_SERVER" to "1", or both.
 // Set "HTTPS_SERVER 1" below to enable http server.
 #define HTTP_PORT 80
 #include <ESP8266WebServer.h>
 ESP8266WebServer serverHttp(HTTP_PORT);
 
-// create clientId / hostname
+// Create clientId / hostname
 String clientIdPrefix = "ESP-MSG-";
 String chipId = String(ESP.getChipId(), HEX);
 //String clientIdTrail = "";
 
-// char lenght of (individually)
+// Char lenght of (individually)
 #define STDSIZE  128
 
 // HTTP and HTTPS Default Authentication to store in config file
 char web_username[STDSIZE] = "admin";
 char web_password[STDSIZE] = "esp8266";
-// allows you to set the realm of authentication Default:"Login Required"
+// Allows you to set the realm of authentication Default:"Login Required"
 const char* www_realm = "Custom Auth Realm";
-// the Content of the HTML response in case of Unautherized Access Default:empty
+// The Content of the HTML response in case of Unautherized Access Default:empty
 String authFailResponse = "Authentication Failed";
 
-const char *webConfigFile  = "/web.config"; // config file
+const char *webConfigFile  = "/web.config";  // Config file
 
 struct webConfigObj {
   char usernameWebHolder[STDSIZE];
   char passwordWebHolder[STDSIZE];
 };
-webConfigObj webConfig; // config object
+webConfigObj webConfig;                      // Config object
 
 WiFiClient espClient;
 PubSubClient mqttClient(espClient);
@@ -90,10 +94,10 @@ char mqttTopicPrefix[STDSIZE] = "rdadotmatrix";
 char mqttTopicDevice[STDSIZE] = "";
 char mqttTopicRoot[STDSIZE] = "";
 
-const unsigned long mqttConnectTimeIntervall = 15000; //15*60*1000; // 15 minutes
+const unsigned long mqttConnectTimeIntervall = 15000;  // 15*60*1000 = 15 minutes
 long mqttLastReconnectAttempt = 0;
 int mqttDisconnected = 0;
-const char *mqttConfigFile  = "/mqtt.config"; // config file
+const char *mqttConfigFile  = "/mqtt.config";          // Config file
 
 struct mqttConfigObj {
   char onOffMqttHolder[STDSIZE];
@@ -106,17 +110,17 @@ struct mqttConfigObj {
   char topicPrefixMqttHolder[STDSIZE];
 };
 
-mqttConfigObj mqttConfig; // config object
+mqttConfigObj mqttConfig;  // Config object
 
 typedef struct {
-  uint8_t spacing;  // character spacing
-  char *msg;  // message to display
+  uint8_t spacing;         // Character spacing
+  char *msg;               // Message to display
 } msgDef_t;
 
 msgDef_t  M[] = { { 1, "" } };
 #define MAX_STRINGS  (sizeof(M)/sizeof(M[0]))
 
-//Message Parameteres size
+// Message Parameteres size
 #define MSG_SIZE 3000
 #define MSG_JSON_SIZE 3000
 #define REP_SIZE 4
@@ -154,7 +158,7 @@ bool newMqttTopicPrefixAvailable = false;
 char mqttStatusMsg[64] = "";
 char mqttAlertMessage[128] = "";
 bool saveMqttConfigAtStart = false;
-bool alertMqttConnect =  false; //not yet implemented on gui, only an hard setting for now
+bool alertMqttConnect =  false;   // Not yet implemented on gui, only an hard setting for now
 char curMessage[MSG_SIZE];
 char newMessage[MSG_SIZE];
 char newRepeat[REP_SIZE];
@@ -192,8 +196,8 @@ String apModeSSID;
 #define PRINTS(s)
 #endif
 
-//uint8_t degC[] = { 5, 3, 3, 56, 68, 68, 68 }; // Deg C
-//uint8_t degF[] = { 6, 3, 3, 124, 20, 20, 4 }; // Deg F
-//uint8_t waveSine[] = { 8, 1, 14, 112, 128, 128, 112, 14, 1 }; // Sine wave
-//uint8_t waveSqar[] = { 8, 1, 1, 255, 128, 128, 128, 255, 1 }; // Square wave
-//uint8_t waveTrng[] = { 10, 2, 4, 8, 16, 32, 64, 32, 16, 8, 4 }; // Triangle wave
+uint8_t degC[] = { 5, 3, 3, 56, 68, 68, 68 };                    // Deg C
+//uint8_t degF[] = { 6, 3, 3, 124, 20, 20, 4 };                    // Deg F
+//uint8_t waveSine[] = { 8, 1, 14, 112, 128, 128, 112, 14, 1 };    // Sine wave
+//uint8_t waveSqar[] = { 8, 1, 1, 255, 128, 128, 128, 255, 1 };    // Square wave
+//uint8_t waveTrng[] = { 10, 2, 4, 8, 16, 32, 64, 32, 16, 8, 4 };  // Triangle wave
